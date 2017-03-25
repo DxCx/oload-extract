f565cf1d44c227fe62a9124807aedbea54f2a35e
Hagai Cohen <hagai.co@gmail.com>
2017-03-25 16:22:18 +0300

    nicefy again

--- openload-extract.js
+++ openload-extract.js
@@ -4,65 +4,62 @@
  */
 function openloadExtract(pageContent) {
   /** @type {number} */
-  var fromIndex = pageContent.indexOf("streamurl") - 18;
-  var progress = pageContent.lastIndexOf(">", fromIndex) + 1;
-
-  var location = pageContent.substring(progress, fromIndex);
-  var currentIndex = location.charCodeAt(0);
-  var fragment = currentIndex - 52;
-  var args = Math.max(2, fragment);
-
-  fragment = Math.min(args, location.length - 30 - 2);
-
-  var scripts = location.substring(fragment, fragment + 30);
-  /** @type {Array} */
-  var parts = new Array(10);
+  var length = pageContent.indexOf("streamurl") - 18;
+  var index = pageContent.lastIndexOf(">", length) + 1;
+  var ol_id = pageContent.substring(index, length);
+  var arrow = ol_id.charCodeAt(0);
   /** @type {number} */
-  var key = 0;
-  while (key < scripts.length) {
-    var camelKey = scripts.substring(key, key + 3);
-    parts[key / 3] = parseInt(camelKey, 8);
-    key += 3;
+  var pos = arrow - 52;
+  /** @type {number} */
+  var nextOpen = Math.max(2, pos);
+  /** @type {number} */
+  pos = Math.min(nextOpen, ol_id.length - 30 - 2);
+  var part = ol_id.substring(pos, pos + 30);
+  /** @type {Array} */
+  var arr = new Array(10);
+  /** @type {number} */
+	var i = 0
+  for (; i < part.length; i += 3) {
+    var id = part.substring(i, i + 3);
+    /** @type {number} */
+    arr[i / 3] = parseInt(id, 8);
   }
-
-  var original = location.split("");
-
-  original.splice(fragment, 30);
-
-  var obj = original.join("");
+  var chars = ol_id.split("");
+  chars.splice(pos, 30);
+  var a = chars.join("");
   /** @type {Array} */
-  var clrs = [];
-
+  var tagNameArr = [];
   /** @type {number} */
-  key = 0;
-
+  i = 0;
   /** @type {number} */
-  for (var widgetPartAttribute = 0; key < obj.length; widgetPartAttribute += 1) {
-    var values = obj.substring(key, key + 2);
-    var value = obj.substring(key, key + 3);
-    var node = obj.substring(key, key + 4);
-    var ret = parseInt(values, 16);
-    key += 2;
-    if (widgetPartAttribute % 3 === 0) {
-      ret = parseInt(value, 8);
-      key += 1;
+  for (var n = 0; i < a.length; n += 1) {
+    var text = a.substring(i, i + 2);
+    var cDigit = a.substring(i, i + 3);
+    var ch = a.substring(i, i + 4);
+    /** @type {number} */
+    var code = parseInt(text, 16);
+    i += 2;
+    if (n % 3 === 0) {
+      /** @type {number} */
+      code = parseInt(cDigit, 8);
+      i += 1;
     } else {
-      if (widgetPartAttribute % 2 === 0) {
-        if (0 !== widgetPartAttribute) {
-          if (obj[widgetPartAttribute - 1].charCodeAt(0) < 60) {
-            ret = parseInt(node, 10);
-            key += 2;
+      if (n % 2 === 0) {
+        if (0 !== n) {
+          if (a[n - 1].charCodeAt(0) < 60) {
+            /** @type {number} */
+            code = parseInt(ch, 10);
+            i += 2;
           }
         }
       }
     }
-    var spaceAfter = parts[widgetPartAttribute % 9];
-    ret ^= 213;
-    ret ^= spaceAfter;
-    clrs.push(String.fromCharCode(ret));
+    var val = arr[n % 9];
+    code ^= 213;
+    code ^= val;
+    tagNameArr.push(String.fromCharCode(code));
   }
-
-  return "https://openload.co/stream/" + clrs.join("") + "?mime=true";
+  return "https://openload.co/stream/" + tagNameArr.join("") + "?mime=true";
 };
 
 module.exports = { openloadExtract: openloadExtract };
